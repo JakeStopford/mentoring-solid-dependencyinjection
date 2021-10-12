a1580456dc74fc561871bfe43ab204992b274d1e
Jake Stopford <Jake_Stopford@hotmail.com>
2021-10-12 23:44:24 +0100

    added readme

--- RocketLeague.Tests/VehicleServiceTests.cs
+++ RocketLeague.Tests/VehicleServiceTests.cs
@@ -1,27 +1,29 @@
-//using System;
-//using NUnit.Framework;
+using Moq;
+using NUnit.Framework;
+using RocketLeague.Data;
 
-//namespace RocketLeague.Tests
-//{
-//    [TestFixture]
-//    internal class VehicleServiceTests
-//    {
-//        [Test]
-//        public void GetAllVehicles_WhenOneVehicleIsAvailable()
-//        {
-//            // Arrange
-//            var vehicleService = new VehicleService();
+namespace RocketLeague.Tests
+{
+    [TestFixture]
+    internal class VehicleServiceTests
+    {
+        [Test]
+        public void GetAllVehicles_WhenOneVehicleIsAvailable()
+        {
+            // Arrange
+            var mock = new Mock<IVehicleRepository>();
+            IVehicleService vehicleService = new VehicleService();
 
-//            // Act
-//            var result = vehicleService.GetAllVehicles();
+            // Act
+            var result = vehicleService.GetAllVehicles();
 
-//            // Assert
+            // Assert
 
-//            /*
-//             * TODO: Modify code to use dependency injection so that assertion below passes
-//             * TODO: Assert that each property of the only vehicle is as expected
-//             */
-//            Assert.That(result.Count == 1);
-//        }
-//    }
-//}
+            /*
+             * TODO: Modify code to use dependency injection so that assertion below passes 
+             * TODO: Assert that each property of the only vehicle is as expected
+             */
+            Assert.That(result.Count == 1);
+        }
+    }
+}
---
+++ RocketLeague.Website/dependency-injection.md
@@ -0,0 +1,14 @@
+Dependency injection is a set of software design principles and patterns that enable us to develop loosely coupled code.
+
+
+Dependency Injection Benefits:
+
+1. Maintainability - Loosely coupled classes are easier to maintain and fix.
+
+2. Testability - Easier to test loosely coupled classes that only do one thing. Creating mocks is easier too. 
+
+3. Readability - Constructors aren’t as cluttered and filled with logic. Classes are more clearly defined, openly declaring what they need.
+
+4. Flexibilty - Small classes that do one thing can more easily be reassembled and reused in different situations.
+
+5. Extensibility - Code that uses dependency injection results in a more extendable class structure. By relying on abstractions instead of implementations, code can easily vary a given implementation.
