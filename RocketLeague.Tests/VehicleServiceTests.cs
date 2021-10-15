bff380f15d73a24b864ee867d6531402798b3400
Jake Stopford <Jake_Stopford@hotmail.com>
2021-10-15 17:30:55 +0100

    fix: fixed tests

--- RocketLeague.Tests/VehicleServiceTests.cs
+++ RocketLeague.Tests/VehicleServiceTests.cs
@@ -1,6 +1,8 @@
 using Moq;
 using NUnit.Framework;
 using RocketLeague.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RocketLeague.Tests
 {
@@ -11,8 +13,20 @@ namespace RocketLeague.Tests
         public void GetAllVehicles_WhenOneVehicleIsAvailable()
         {
             // Arrange
-            var mock = new Mock<IVehicleRepository>();
-            IVehicleService vehicleService = new VehicleService();
+            var mockVehicles = new List<Vehicle>
+            {
+                new Vehicle
+                {
+                    Name = "Breakout",
+                    Hitbox = "Breakout",
+                    Description = "Breakout is a battle-car released on July 7, 2015, with the release of the game. It can be unlocked by completing matches. It is classified as \"Common\" rarity.",
+                    ReleaseDate = "2015-07-07"
+                }
+            };
+
+            var vehicleRepository = new Mock<IVehicleRepository>();
+            vehicleRepository.Setup(x => x.GetVehicles()).Returns(mockVehicles);
+            var vehicleService = new VehicleService(vehicleRepository.Object);
 
             // Act
             var result = vehicleService.GetAllVehicles();
@@ -24,6 +38,10 @@ namespace RocketLeague.Tests
              * TODO: Assert that each property of the only vehicle is as expected
              */
             Assert.That(result.Count == 1);
+            Assert.That(result.First().Description ==
+                        "Breakout is a battle-car released on July 7, 2015, with the release of the game. It can be unlocked by completing matches. It is classified as \"Common\" rarity.");
+            Assert.That(result.First().Hitbox == "Breakout");
+            Assert.That(result.First().ReleaseDate == "2015-07-07");
         }
     }
 }
